40ea9f527b9c71ded9327d664b238deb7561c45e
Margot <franziska128@online.de>
2024-04-25 13:17:54 +0200

    review stuff drin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,15 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Plapy",
+  description: "Website for the Plapy Discord Bot",
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -12,4 +23,5 @@ export default function RootLayout({
     </ClerkProvider>
   )
 }
+
 
